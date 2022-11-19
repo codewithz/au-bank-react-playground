1e91cf76260f55c54df9ee5470ed688fe492d40c
Zartab M Nakhwa <zartab@codewithz.com>
2022-11-19 17:13:27 +0530

    Props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { HelloComponent } from "./components/HelloComponent";
+import { HelloFunctionalComponent } from "./components/HelloFunctionalComponent";
+import { NameComponent } from "./components/NameComponent";
+import { NameFunctionalComponent } from "./components/NameFunctionalComponent";
 
 export default function App() {
   return (
@@ -8,6 +11,16 @@ export default function App() {
       <h1>Welcome to AU Small Finance Bank</h1>
       <hr />
       <HelloComponent />
+      <hr />
+      <HelloFunctionalComponent />
+      <hr />
+      <NameComponent name="Zartab" message="Keep Coding!!" />
+      <hr />
+      <NameComponent name="AU Bank" message="Badlav Mumkin Hai" />
+      <hr />
+      <NameComponent name="Tom" msg="Hello World!!" />
+      <hr />
+      <NameFunctionalComponent name="Alex" message="Save Water!!" />
     </div>
   );
 }
