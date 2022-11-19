f759e1c9262e9dccaf1ae88e07b21aac34647d4e
Zartab M Nakhwa <zartab@codewithz.com>
2022-11-19 16:28:18 +0530

    Initial Commit

---
+++ src/components/HelloComponent.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class HelloComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Component</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet,
+          est?
+        </p>
+      </div>
+    );
+  }
+}
